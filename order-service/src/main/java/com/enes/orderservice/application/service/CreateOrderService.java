29f9e3cfef246c61c1bf4cdf092c5da1d2e2d906
enesincekaraa <enesincekara61@gmail.com>
2026-03-19 23:43:47 +0300

    feat: implement order creation flow with clean architecture

--- order-service/src/main/java/com/enes/orderservice/application/service/CreateOrderService.java
+++ order-service/src/main/java/com/enes/orderservice/application/service/CreateOrderService.java
@@ -1,22 +1,28 @@
 package com.enes.orderservice.application.service;
 
 import com.enes.orderservice.application.dto.CreateOrderRequest;
+import com.enes.orderservice.domain.event.OrderCreatedEvent;
 import com.enes.orderservice.domain.model.Order;
 import com.enes.orderservice.domain.repository.OrderRepository;
+import com.enes.orderservice.infrastructure.kafka.OrderEventProducer;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 
 @Service
+
 public class CreateOrderService {
     private final OrderRepository orderRepository;
+    private final OrderEventProducer producer;
 
-    public CreateOrderService(OrderRepository orderRepository) {
+    public CreateOrderService(OrderRepository orderRepository, OrderEventProducer producer) {
         this.orderRepository = orderRepository;
+        this.producer = producer;
     }
 
     public Order create(CreateOrderRequest req){
         Order order = new Order(req.amount());
-        return orderRepository.save(order);
+        Order saved = orderRepository.save(order);
+        producer.send(new OrderCreatedEvent(saved.getId(), saved.getTotalAmount()));
+        return saved;
     }
 }
