e71514d52b0ad3cc12ae52e1088097f93fc0f37c
enesincekaraa <enesincekara61@gmail.com>
2026-03-19 14:48:48 +0300

    feat: initialize order-service with Spring Boot structure

---
+++ order-service/src/test/java/com/enes/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enes.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
